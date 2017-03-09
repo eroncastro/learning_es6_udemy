2df991ce33e3f04df5da4e4bbfd41f626e049cfa
Eron <eroncastro@gmail.com>
2017-03-09 08:41:19 -0300

    Adds Section 4 Lecture 10

---
+++ Section 4/Lecture 10 - Selecting needed data with filter/index.js
@@ -0,0 +1,25 @@
+var products = [
+  { name: 'cucumber', type: 'vegetable' },
+  { name: 'banana', type: 'fruit' },
+  { name: 'celery', type: 'vegetable' },
+  { name: 'orange', type: 'fruit' },
+];
+
+// ES5 approach for finding all fruit objects
+var filteredProducts = [];
+
+for (var i = 0; i < products.length; i++) {
+  if (products[i].type === 'fruit') {
+    filteredProducts.push(products[i]);
+  }
+}
+
+console.log(filteredProducts);
+
+// ES6 approach for finding all fruit objects
+
+var filtered = products.filter(function(product) {
+  return product.type === 'fruit';
+});
+
+console.log(filtered);
