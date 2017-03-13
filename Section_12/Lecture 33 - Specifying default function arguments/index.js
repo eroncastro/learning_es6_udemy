8a1302d9e78f2732a6a3b68c3fe5a84e0d355881
Eron <eroncastro@gmail.com>
2017-03-13 14:44:12 -0300

    Adds Section 12

---
+++ Section_12/Lecture 33 - Specifying default function arguments/index.js
@@ -0,0 +1,9 @@
+function makeAjaxRequest(url, method = 'GET') {
+  console.log(method);
+
+  // logic to make the request
+}
+
+makeAjaxRequest('google.com');
+makeAjaxRequest('google.com', 'GET');
+makeAjaxRequest('google.com', 'POST');
---
+++ Section_12/Lecture 34 - Use cases of defaulting arguments/index.js
@@ -0,0 +1,17 @@
+function User(id) {
+  this.id = id;
+}
+
+function generateId() {
+  return Math.random() * 9999999;
+}
+
+function createAdminuser(user = new User(generateId())) {
+  user.admin = true;
+
+  return user;
+}
+
+let user = new User(generateId());
+createAdminuser(user);
+console.log(user).
