df970180ea6608c2b28b756a041c9b78e71af2ac
Eron <eroncastro@gmail.com>
2017-03-14 19:01:14 -0300

    Adds Lecture 52

--- Section_16/Lecture 52 - Another step in our generator story/index.js
+++ Section_16/Lecture 52 - Another step in our generator story/index.js
@@ -6,11 +6,15 @@ function* shopping() {
   //  go into the store with cash
   const stuffFromStore = yield 'cash';
 
+  // walking to laundry place
+  const cleanClothes = yield 'laundry';
+
   // walking back home
-  return stuffFromStore;
+  return [stuffFromStore, cleanClothes];
 }
 
 // stuff in the store
 const gen = shopping();
 console.log(gen.next()); // leaving our house
+console.log(gen.next('clothes'));
 console.log(gen.next('groceries')); // leaving the store with groceries
