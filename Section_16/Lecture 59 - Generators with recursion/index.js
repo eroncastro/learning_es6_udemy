abdd827a07e43d9f36f2266750cbf767e3b927c6
Eron <eroncastro@gmail.com>
2017-03-15 09:32:05 -0300

    Adds Lectures 59 and 60

---
+++ Section_16/Lecture 59 - Generators with recursion/index.js
@@ -0,0 +1,16 @@
+class Comment {
+  constructor(content, children) {
+    this.content = content;
+    this.children = children;
+  }
+}
+
+const children = [
+  new Comment('good comment', []),
+  new Comment('bad comment', []),
+  new Comment('meh', []),
+]
+
+const tree = new Comment('Great post!', children);
+
+console.log(tree);
